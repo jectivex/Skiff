92f1bd5ae787f5aba1e014670f1884c599be5510
marcprux <mwp1@cornell.edu>
2023-01-30 16:14:41 -0500

    Add SymbolKit

--- Package.swift
+++ Package.swift
@@ -12,11 +12,13 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/jectivex/Kanji.git", from: "1.1.3"),
         .package(url: "https://github.com/jectivex/Gryphon.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-docc-symbolkit.git", branch: "main"),
         //.package(url: "https://github.com/jectivex/Gryphon.git", from: "0.2.2"), // 'gryphon' >= 0.2.0 cannot be used because no versions of 'gryphon' match the requirement 0.2.1..<1.0.0 and package 'gryphon' is required using a stable-version but 'gryphon' depends on an unstable-version package 'swift-syntax'.
     ],
     targets: [
         .target(name: "Skiff", dependencies: [
             .product(name: "GryphonLib", package: "Gryphon", condition: .when(platforms: [.macOS, .linux])),
+            .product(name: "SymbolKit", package: "swift-docc-symbolkit", condition: .when(platforms: [.macOS, .linux])),
         ]),
         .testTarget(name: "SkiffTests", dependencies: [
             "Skiff",
